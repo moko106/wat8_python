5730542474df2f4ef5d4edc09945546f9b2429c2
moko106 <konsta.monkkonen@edu.hyvinkaa.fi>
2026-03-28 17:47:28 +0200

    Create tehtava3.py

---
+++ tehtavat/01_perusteet/tehtava3.py
@@ -0,0 +1,15 @@
+# Tehtävä 4: Summa
+
+def summa(a, b):
+    # Palauta lukujen summa
+    pass
+
+
+def test_summa():
+    assert summa(2, 3) == 5
+    assert summa(-1, 1) == 0
+
+
+if __name__ == "__main__":
+    test_summa()
+    print("OK!")
