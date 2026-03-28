2080ffd4d0102ce967d9e2fc0d01f83957dc141c
moko106 <konsta.monkkonen@edu.hyvinkaa.fi>
2026-03-28 18:04:58 +0200

    Create tehtava4.py

---
+++ tehtavat/01_perusteet/tehtava4.py
@@ -0,0 +1,15 @@
+# Tehtävä 4: Luku on parillinen
+
+def parillinen(luku):
+    # Palauta tieto, onko luku parillinen
+    pass
+
+
+def test_parillinen():
+    assert parillinen(10) == True
+    assert parillinen(31) == False
+
+
+if __name__ == "__main__":
+    test_parillinen()
+    print("OK!")
