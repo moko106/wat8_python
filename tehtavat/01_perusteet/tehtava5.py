e8d17ce40e5e90f986f71f607d29f3ec6159650d
moko106 <konsta.monkkonen@edu.hyvinkaa.fi>
2026-04-13 20:51:02 +0300

    Add files via upload

---
+++ tehtavat/01_perusteet/tehtava5.py
@@ -0,0 +1,13 @@
+# Tehtävä 5: Kirjoita funktio, joka palauttaa piin likiarvon 3.14159
+
+def pii():
+    pass
+
+
+def test():
+    assert pii() == 3.14159
+
+
+if __name__ == "__main__":
+    test()
+    print("OK!")
